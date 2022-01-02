0da6c031d8bbde602e0599aa71ff7def1cb5c6b7
Adwin Ying <adwinying@gmail.com>
2022-01-02 21:56:30 +0900

    Init db seeder and client

---
+++ app/utils/db.server.ts
@@ -0,0 +1,27 @@
+/* eslint-disable no-underscore-dangle */
+import { PrismaClient } from "@prisma/client";
+
+let client: PrismaClient;
+
+declare global {
+  // eslint-disable-next-line
+  var __db: PrismaClient | undefined;
+}
+
+// this is needed because in development we don't want to restart
+// the server with every change, but we want to make sure we don't
+// create a new connection to the DB with every change either.
+if (process.env.NODE_ENV === "production") {
+  client = new PrismaClient();
+  client.$connect();
+} else {
+  if (!global.__db) {
+    global.__db = new PrismaClient();
+    global.__db.$connect();
+  }
+  client = global.__db;
+}
+
+export const db = client;
+
+export default null;
---
+++ prisma/seed/user.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+
+const db = new PrismaClient();
+
+const users = [
+  {
+    name: "Admin",
+    email: "admin@example.org",
+    password: "$2y$10$fQ/fazS6NSwUfY7/lauARuiuE/7cZEjrdpuvF4PK6J18Hx14UbLMK", // secret
+  },
+];
+
+(async () => {
+  await Promise.all(users.map((user) => db.user.create({ data: user })));
+})();
