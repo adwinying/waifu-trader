6d4d0a46a44a62b2a7795e486dec3e608200543f
Adwin Ying <adwinying@gmail.com>
2022-02-12 17:23:32 +0900

    Add logout link to header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,4 +1,4 @@
-import { UserCircleIcon } from "@heroicons/react/solid";
+import { CogIcon, LogoutIcon, UserCircleIcon } from "@heroicons/react/solid";
 import { Link } from "remix";
 import logo from "~/assets/img/logo.jpg";
 import gemIcon from "~/assets/img/gem.svg";
@@ -26,10 +26,39 @@ export default function Header({ userName, points }: Props) {
                 <img src={gemIcon} alt="Gem Icon" className="inline h-6 mr-1" />
                 <span cy-data="header-points">{points?.toLocaleString()}</span>
               </Link>
-              <Link to="/preferences" className="hover:opacity-80">
-                <UserCircleIcon className="inline w-8 mr-1" />
-                <span cy-data="header-user-name">{userName}</span>
-              </Link>
+              <div className="dropdown dropdown-end">
+                <div
+                  // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
+                  tabIndex={0}
+                >
+                  <UserCircleIcon className="inline w-8 mr-1" />
+                  <span cy-data="header-user-name">{userName}</span>
+                </div>
+                <ul
+                  // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
+                  tabIndex={0}
+                  className="mt-2 p-2 shadow-lg menu dropdown-content bg-base-100 rounded-box w-52"
+                >
+                  <li>
+                    <Link to="/preferences">
+                      <CogIcon className="inline w-5 h-5 mr-1" />
+                      <span>Preferences</span>
+                    </Link>
+                  </li>
+                  <form action="/logout" method="POST">
+                    <li>
+                      <button
+                        type="submit"
+                        className="px-5 btn btn-outline btn-error justify-start border-0
+                          font-normal normal-case text-base"
+                      >
+                        <LogoutIcon className="inline w-5 h-5 mr-1" />
+                        <span>Logout</span>
+                      </button>
+                    </li>
+                  </form>
+                </ul>
+              </div>
             </>
           ) : (
             <>
