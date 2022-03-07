616321b3cfd5c92912db599b4cb10b21562e388a
Adwin Ying <adwinying@gmail.com>
2022-03-07 23:02:43 +0900

    Implement waifu claim feature

--- app/routes/waifus.tsx
+++ app/routes/waifus.tsx
@@ -1,14 +1,128 @@
-import { MetaFunction } from "remix";
+import { RefreshIcon } from "@heroicons/react/solid";
+import {
+  ActionFunction,
+  Form,
+  LoaderFunction,
+  MetaFunction,
+  redirect,
+  useLoaderData,
+  useTransition,
+} from "remix";
+import GemIcon from "~/components/icons/GemIcon";
 import PageTitle from "~/components/PageTitle";
+import claimUnclaimedWaifu from "~/libs/claimUnclaimedWaifu";
+import getUserWaifuClaimCost from "~/libs/getUserWaifuClaimCost";
+import getUserWaifuCount from "~/libs/getUserWaifuCount";
+import updateUserPoints from "~/libs/updateUserPoints";
+import { requireUserSession } from "~/utils/auth.server";
+import { commitSession, getSession } from "~/utils/session.server";
 
 export const meta: MetaFunction = () => ({
   title: "Waifus - Waifu Trader",
 });
 
+type LoaderData = {
+  waifuClaimCost: number;
+  waifuCount: number;
+  canClaimWaifu: boolean;
+};
+export const loader: LoaderFunction = async ({ request }) => {
+  const user = await requireUserSession(request);
+
+  const waifuClaimCost = await getUserWaifuClaimCost({ user });
+  const waifuCount = await getUserWaifuCount({ user });
+  const pointBalance = user.points;
+  const canClaimWaifu = waifuClaimCost <= pointBalance;
+
+  return {
+    waifuClaimCost,
+    waifuCount,
+    canClaimWaifu,
+  };
+};
+
+export const action: ActionFunction = async ({ request }) => {
+  const user = await requireUserSession(request);
+  const session = await getSession(request.headers.get("cookie"));
+  const waifuCost = await getUserWaifuClaimCost({ user });
+  const reason = "Claimed waifu";
+
+  try {
+    await updateUserPoints({ user, pointChange: -waifuCost, reason });
+    await claimUnclaimedWaifu({ user });
+  } catch (err) {
+    const approvedErrors = [
+      "Insufficient points",
+      "No unclaimed waifus available",
+    ];
+
+    session.flash("notification", {
+      type: "error",
+      message:
+        err instanceof Error && approvedErrors.includes(err.message)
+          ? err.message
+          : "Failed to claim waifu",
+    });
+
+    return redirect(request.url, {
+      headers: {
+        "Set-Cookie": await commitSession(session),
+      },
+    });
+  }
+
+  session.flash("notification", {
+    type: "success",
+    message: "Successfully claimed waifu.",
+  });
+
+  return redirect(request.url, {
+    headers: {
+      "Set-Cookie": await commitSession(session),
+    },
+  });
+};
+
 export default function Waifus() {
+  const { waifuCount, waifuClaimCost, canClaimWaifu } =
+    useLoaderData<LoaderData>();
+
+  const transition = useTransition();
+  const isSubmitting = transition.state === "submitting";
+
   return (
     <div>
       <PageTitle>Waifus</PageTitle>
+
+      <div className="flex items-center space-x-3">
+        <h2 className="text-3xl font-bold">
+          My Waifus <span cy-data="userWaifuCount">({waifuCount})</span>
+        </h2>
+
+        <Form method="post" className="text-center">
+          <button
+            cy-data="claimWaifuBtn"
+            className="btn btn-primary btn-sm"
+            disabled={!canClaimWaifu}
+            type="submit"
+          >
+            {isSubmitting ? (
+              <>
+                <RefreshIcon className="mr-2 h-5 w-5 animate-spin" />
+                Claiming...
+              </>
+            ) : (
+              <>
+                Claim New Waifu for
+                <GemIcon className="ml-1 mr-0.5 h-3.5 w-3.5" />
+                <span cy-data="waifuCost">
+                  {waifuClaimCost.toLocaleString()}
+                </span>
+              </>
+            )}
+          </button>
+        </Form>
+      </div>
     </div>
   );
 }
---
+++ cypress/integration/waifus.spec.ts
@@ -0,0 +1,124 @@
+describe("waifus", () => {
+  beforeEach(() => {
+    cy.setupDb();
+  });
+
+  it("should redirect to login when unauthenticated", () => {
+    const redirectUrl = `${Cypress.config().baseUrl}/waifus`;
+
+    cy.visit("/waifus");
+    cy.url().should(
+      "eq",
+      `${Cypress.config().baseUrl}/login?redirect=${encodeURIComponent(
+        redirectUrl,
+      )}`,
+    );
+  });
+
+  it("should show error if no waifus can be claimed ", () => {
+    const email = "foo@bar.com";
+    const points = 50;
+
+    cy.login({ email, points });
+    cy.visit("/waifus");
+
+    cy.get('[cy-data="headerPoints"]').should(
+      "contain.text",
+      points.toLocaleString(),
+    );
+    cy.get('[cy-data="claimWaifuBtn"]').should("not.have.attr", "disabled");
+    cy.get('[cy-data="waifuCost"]').should("contain.text", "50");
+    cy.get('[cy-data="claimWaifuBtn"]').click();
+
+    cy.url().should("eq", `${Cypress.config().baseUrl}/waifus`);
+    cy.get('.alert.alert-error [cy-data="notificationTitle"]').should(
+      "contain.text",
+      "No unclaimed waifus available",
+    );
+  });
+
+  it("should show error if insufficient points to claim waifu", () => {
+    const email = "foo@bar.com";
+    const points = 0;
+
+    cy.login({ email, points });
+    cy.visit("/waifus", { method: "POST" });
+
+    cy.url().should("eq", `${Cypress.config().baseUrl}/waifus`);
+    cy.get('.alert.alert-error [cy-data="notificationTitle"]').should(
+      "contain.text",
+      "Insufficient points",
+    );
+  });
+
+  it("should disable claim waifu button if insufficient points", () => {
+    const email = "foo@bar.com";
+    const points = 0;
+
+    cy.login({ email, points });
+    cy.visit("/waifus");
+
+    cy.get('[cy-data="headerPoints"]').should(
+      "contain.text",
+      points.toLocaleString(),
+    );
+    cy.get('[cy-data="claimWaifuBtn"]').should("have.attr", "disabled");
+  });
+
+  it("should be able to claim waifus", () => {
+    const email = "foo@bar.com";
+    const points = 50 * 9 + 100;
+    const waifu = new Array(10).fill(null).map((_, i) => ({
+      name: `Waifu${i}`,
+      series: "Series",
+      description: "Some description",
+      img: "http://example.org/baz.jpg",
+    }));
+
+    cy.seedDb({ waifu });
+    cy.login({ email, points });
+    cy.visit("/waifus");
+
+    cy.get('[cy-data="headerPoints"]').should(
+      "contain.text",
+      points.toLocaleString(),
+    );
+    cy.get('[cy-data="claimWaifuBtn"]').should("not.have.attr", "disabled");
+    cy.get('[cy-data="waifuCost"]').should("contain.text", "50");
+    cy.get('[cy-data="userWaifuCount"]').should("contain.text", "0");
+    cy.get('[cy-data="claimWaifuBtn"]').click();
+
+    cy.url().should("eq", `${Cypress.config().baseUrl}/waifus`);
+    cy.get('.alert.alert-success [cy-data="notificationTitle"]').should(
+      "contain.text",
+      "Successfully claimed waifu.",
+    );
+    cy.get('[cy-data="headerPoints"]').should("contain.text", 500);
+    cy.get('[cy-data="userWaifuCount"]').should("contain.text", "1");
+  });
+
+  it("should show correct amount of points to claim waifu", () => {
+    const email = "foo@bar.com";
+    const waifus = new Array(100).fill(null).map((_, i) => ({
+      name: `Waifu${i}`,
+      series: "Series",
+      description: "Some description",
+      img: "http://example.org/baz.jpg",
+    }));
+
+    cy.seedDb({
+      user: [
+        {
+          username: "foo",
+          email,
+          password: "password",
+          waifus: { createMany: { data: waifus } },
+        },
+      ],
+    });
+    cy.login({ email });
+    cy.visit("/waifus");
+
+    cy.get('[cy-data="waifuCost"]').should("contain.text", "51,200");
+  });
+});
