5042efdc85d6c28fd08a7ce7f1b8b812fda4262c
Adwin Ying <adwinying@gmail.com>
2022-03-13 19:05:45 +0900

    Add basic hero to landing page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,39 +1,35 @@
+import { Link, LoaderFunction, useLoaderData } from "remix";
+import { getAuthUser } from "~/utils/auth.server";
+
+type LoaderData = {
+  isLoggedIn: boolean;
+};
+export const loader: LoaderFunction = async ({ request }) => {
+  const user = await getAuthUser(request);
+
+  return {
+    isLoggedIn: user !== null,
+  };
+};
+
 export default function Index() {
+  const { isLoggedIn } = useLoaderData<LoaderData>();
+
   return (
-    <div>
-      <h1 className="text-2xl">Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-            className="link"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-            className="link"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-            className="link"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="my-8 flex flex-col items-center space-y-4 md:space-y-6">
+      <h1 className="text-center text-4xl font-bold md:text-5xl">
+        Collect your precious waifus
+      </h1>
+
+      {isLoggedIn ? (
+        <Link to="/waifus" className="btn btn-primary">
+          My Waifus
+        </Link>
+      ) : (
+        <Link to="/signup" className="btn btn-primary">
+          Sign Up
+        </Link>
+      )}
     </div>
   );
 }
